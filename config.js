d93a1e3cf1a910210d44e35114c41637403e929b
hrfmmymt <hrfmmymt@gmail.com>
2016-11-17 00:49:32 +0900

    show simple webview.

---
+++ config.js
@@ -0,0 +1,11 @@
+const Config = require('electron-config');
+
+module.exports = new Config({
+  defaults: {
+    zoomFactor: 1,
+    lastWindowState: {
+      width: 960,
+      height: 320
+    }
+  }
+});
